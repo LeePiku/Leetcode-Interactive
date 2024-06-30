75396f173c25373c618c25fa519e7a2c250e6dc6
LeePiku <lhphuoc104@gmail.com>
2024-06-30 18:38:55 +0700

    Create 58. Length of Last Word - lengthOfLastWord.py

---
+++ 150interview/Array&String/58. Length of Last Word - lengthOfLastWord.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        result = s.split()
+        return len(result[-1])
